b7b0e269b40d768f191980491a932701b0e8bd67
Maxime Morard <maxmjm@icloud.com>
2025-01-24 16:07:54 +0800

    Take fields from Contact and convert to json for passing to API

--- backend/models.py
+++ backend/models.py
@@ -5,3 +5,12 @@ class Contact(db.Model):
     first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email
+        }
+    
