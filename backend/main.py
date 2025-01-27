671d1e326644a016860a9397467f57a8e1c5a567
Maxime Morard <maxmjm@icloud.com>
2025-01-27 10:36:52 +0800

    Function with decorator to return (get) a list of contact objects in json format

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+@app.route("/contacts", methods=["GET"]) # Function decorator
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts}) # Return list of contact objects in json format
+
 # Entry point of program
 if __name__ == "__main__":
     with app.app_context():
