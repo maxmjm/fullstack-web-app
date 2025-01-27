f11be53d77f819c34f81c3ababdf3320d199490e
Maxime Morard <maxmjm@icloud.com>
2025-01-27 17:11:14 +0800

    Created ContactForm with data requests and form html

---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const ContactForm = ({}) => {
+  // Variable states
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Define data
+    const data = {
+      firstName,
+      lastName,
+      email,
+    };
+
+    // Define url endpoint
+    const url = "http://127.0.0.1:5000/create_contact";
+
+    // Set options for request
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    // Set request
+    const response = await fetch(url, options);
+
+    // Check if request is successful
+    if (response.status != 201 && response.status != 200) {
+      const message = await response.json();
+      alert(data.message);
+    } else {
+      // Successful
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit">Create Contact</button>
+    </form>
+  );
+};
