dc9457edafd6f6191f022d61ad11f9a4183dcc8c
Maxime Morard <maxmjm@icloud.com>
2025-01-28 13:40:11 +0800

    Added a modal menu to create a new contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import ContactList from "./ContactList";
 import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     // Fetch contacts when application starts up
@@ -17,10 +19,25 @@ function App() {
     console.log(data.contacts);
   };
 
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
   return (
     <>
       <ContactList contacts={contacts} />
-      <ContactForm />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      { isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm />
+        </div>
+      </div>
+      }
     </>
   );
 }
